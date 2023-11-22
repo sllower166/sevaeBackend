b19efe4ff2d9ec2258075f8b42d8db5fb14bfb00
sllower166 <git>
2023-11-22 01:18:02 -0500

    env wsport

--- websocket/websocket.js
+++ websocket/websocket.js
@@ -1,9 +1,9 @@
 const WebSocket = require("ws");
 const { processMsg } = require("./processMsg");
-const WS_PORT = process.env.WS_PORT || 6660;
 let io;
-function initializeWebSocket(server) {
-  io = new WebSocket.Server({ server });
+function initializeWebSocket() {
+  const WS_PORT = process.env.WS_PORT;
+  io = new WebSocket.Server({ port: WS_PORT });
   console.log(io);
   io.on("connection", (socket) => {
     console.log("Cliente WebSocket conectado.");
