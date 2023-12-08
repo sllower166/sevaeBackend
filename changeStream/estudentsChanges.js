87cab1457259b1afffd393172da685f9ca237433
sllower166 <git>
2023-12-08 13:27:58 -0500

    Mejoras en changeStream de registros

--- changeStream/estudentsChanges.js
+++ changeStream/estudentsChanges.js
@@ -1,7 +1,6 @@
 const {
   notificarEmailRegistrosEstudiante,
 } = require("../mailSender/mailSender.js");
-
 const Estudiante = require("../models/Estudiante.js");
 const {
   notificarSmsRegistrosEstudiante,
@@ -12,26 +11,40 @@ const startStudentsChanges = () => {
   const TWILIO_ACTIVATE = process.env.TWILIO_ACTIVATE;
 
   changeStream.on("change", async (change) => {
-    if (
-      change.operationType === "update" &&
-      Object.keys(change.updateDescription.updatedFields).some((field) =>
-        field.startsWith("registros")
-      )
-    ) {
-      registroAgg = change.updateDescription.updatedFields;
+    const { operationType, updateDescription, documentKey } = change;
+
+    const updatedFields = updateDescription?.updatedFields || {};
+    const hasRegistroUpdates = Object.keys(updatedFields).some((field) =>
+      field.startsWith("registros")
+    );
+
+    if (operationType === "update" && hasRegistroUpdates) {
       try {
-        const keyName = Object.keys(registroAgg).find((field) =>
-          field.startsWith("registros")
+        let registroId;
+
+        for (const key in updatedFields) {
+          if (key.startsWith("registros")) {
+            const registro = Array.isArray(updatedFields[key])
+              ? updatedFields[key][0]
+              : updatedFields[key];
+            registroId = registro._id;
+            break;
+          }
+        }
+
+        const studentId = documentKey._id;
+        const updatedStudent = await Estudiante.findById(studentId);
+        const updatedRegistro = updatedStudent.registros.find((registro) =>
+          registro._id.equals(registroId)
         );
 
-        const registro = registroAgg[keyName];
+        notificarEmailRegistrosEstudiante(updatedStudent, updatedRegistro);
 
-        const studentId = change.documentKey._id;
-        const updatedStudent = await Estudiante.findById(studentId);
-
-        notificarEmailRegistrosEstudiante(updatedStudent, registro);
         if (TWILIO_ACTIVATE === "true") {
-          await notificarSmsRegistrosEstudiante(updatedStudent, registro);
+          await notificarSmsRegistrosEstudiante(
+            updatedStudent,
+            updatedRegistro
+          );
         }
       } catch (error) {
         console.error("Error al obtener el estudiante:", error);
